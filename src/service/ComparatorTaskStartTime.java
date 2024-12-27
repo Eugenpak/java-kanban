3d7392f9ac367b0e3c57f2e17863abe98865e4bb
Eugenpak <techteamit@yandex.ru>
2024-12-27 06:47:45 +0300

    CheckStyle-6

--- src/service/ComparatorTaskStartTime.java
+++ src/service/ComparatorTaskStartTime.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 
 public class ComparatorTaskStartTime implements Comparator<Task> {
     @Override
-    public int compare(Task a, Task b){
+    public int compare(Task a, Task b) {
         if (a.getStartTime().isAfter(b.getStartTime())) {
             return 1;
         } else if (a.getStartTime().isBefore(b.getStartTime())) {
